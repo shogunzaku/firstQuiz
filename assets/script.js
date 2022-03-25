f222909d15733fc28b160e99a5e5b714187de7c7
Shogun <shogunzaku@gmail.com>
2022-03-24 21:18:49 -0700

    added question images, questions, question intelligence and quiz progress

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,114 @@ const counter = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
+
+// Questions for the quiz 
+let questions = [
+    {
+        question: "Does html mean hyper text mark lasers?",
+        imgSrc: "assets/img/html5.png",
+        choiceA: "Nah!!!",
+        choiceB: "Yes!!!",
+        choiceC: "IDK!!!",
+        correct: "A",
+    }, {
+        question: "Is CSS even needed for a web page?",
+        imgSrc: "assets/img/css3.png",
+        choiceA: "never!",
+        choiceB: "yup!",
+        choiceC: "sometimes!",
+        correct: "B"
+    }, {
+        question: "Is JS the same thing as Java?",
+        imgSrc: "assets/img/js.png",
+        choiceA: "all the same",
+        choiceB: "uuuh...",
+        choiceC: "NOPE!",
+        correct: "C"
+    }
+];
+
+// Created variables
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+
+// render question
+function renderQuestion(){
+    let q = questions[runningQuestion];
+
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+start.addEventListener("click", startQuiz);
+
+// starts quiz
+function startQuiz() {
+start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
+}
+// render progress
+function renderProgress() {
+    for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+        progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+    }
+}
+// counter render
+
+function renderCounter() {
+    if (count <= questionTime) {
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    } else {
+        count = 0;
+        // change progress color to red
+        answerIsWrong();
+        if (runningQuestion < lastQuestion) {
+            runningQuestion++;
+            renderQuestion();
+        } else {
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+// checks answer
+function checkAnswer(answer) {
+    if (answer == questions[runningQuestion].correct) {
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    } else {
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if (runningQuestion < lastQuestion) {
+        runningQuestion++;
+        renderQuestion();
+    } else {
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+// answer is correct
+function answerIsCorrect() {
+    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+
+// answer is Wrong
+function answerIsWrong() {
+    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
