f129ce840e87d06a8f13af42138d947cc57ad8b2
Shogun <shogunzaku@gmail.com>
2022-03-24 22:12:37 -0700

    game time, progress bar active and image score results display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// selecting all my elements
+//  selected all my elements
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
@@ -11,25 +11,26 @@ const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
 
-// Questions for the quiz 
+
+// Questions for quiz
 let questions = [
     {
         question: "Does html mean hyper text mark lasers?",
-        imgSrc: "assets/img/html5.png",
+        imgSrc: "img/html5.png",
         choiceA: "Nah!!!",
         choiceB: "Yes!!!",
         choiceC: "IDK!!!",
         correct: "A",
     }, {
         question: "Is CSS even needed for a web page?",
-        imgSrc: "assets/img/css3.png",
+        imgSrc: "img/css3.png",
         choiceA: "never!",
         choiceB: "yup!",
         choiceC: "sometimes!",
         correct: "B"
     }, {
         question: "Is JS the same thing as Java?",
-        imgSrc: "assets/img/js.png",
+        imgSrc: "img/js.png",
         choiceA: "all the same",
         choiceB: "uuuh...",
         choiceC: "NOPE!",
@@ -37,26 +38,32 @@ let questions = [
     }
 ];
 
-// Created variables
+//  variables
+
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; // 10s
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+let score = 0;
 
 // render question
 function renderQuestion(){
     let q = questions[runningQuestion];
-
+    
     question.innerHTML = "<p>"+ q.question +"</p>";
     qImg.innerHTML = "<img src="+ q.imgSrc +">";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
 }
-
 start.addEventListener("click", startQuiz);
 
-// starts quiz
+// start quiz
 function startQuiz() {
-start.style.display = "none";
+    start.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
     renderProgress();
@@ -90,7 +97,7 @@ function renderCounter() {
         }
     }
 }
-// checks answer
+// check anwer
 function checkAnswer(answer) {
     if (answer == questions[runningQuestion].correct) {
         // answer is correct
@@ -121,3 +128,21 @@ function answerIsCorrect() {
 function answerIsWrong() {
     document.getElementById(runningQuestion).style.backgroundColor = "#f00";
 }
+// score render
+function scoreRender() {
+    scoreDiv.style.display = "block";
+
+    // calculate the amount of question percent answered by player
+    const scorePerCent = Math.round(100 * score / questions.length);
+
+    // choose the image based on the score
+    let img = (scorePerCent >= 80) ? "img/kekw.png" :
+        (scorePerCent >= 60) ? "img/poggers.png" :
+            (scorePerCent >= 40) ? "img/charlierage.png" :
+                (scorePerCent >= 20) ? "img/kekwait1.png" :
+                    "img/pepehands.png";
+
+    scoreDiv.innerHTML = "<img src=" + img + ">";
+    scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
+}
+
